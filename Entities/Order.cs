d2a5ea80e4c888962716173b0bd0dbc86a62916b
Maxuell Chaves <maxuellchaves30@gmail.com>
2020-04-14 22:05:58 -0300

    programa finalizado e executado

---
+++ Entities/Order.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Composition_obj_exer_3.Entities.Enums;
+using System.Globalization;
+namespace Composition_obj_exer_3.Entities
+{
+    class Order
+    {
+        public DateTime Moment{ get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem (OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem obj in Items)
+            {
+                sum += obj.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder os = new StringBuilder();
+            os.AppendLine("Order moment: " + Moment);
+            os.AppendLine("Order status: " + Status);
+            os.AppendLine("Client: " + Client.Name + " (" + Client.BirthDate.ToString("dd/MM/yyyy") + ") " + " - " + Client.Email);
+            os.AppendLine("\nOrder items:");
+
+            foreach(OrderItem item in Items)
+            {
+                os.AppendLine(item.ToString());
+            }
+            os.AppendLine("Total price: $" + Total().ToString("F2",CultureInfo.InvariantCulture));
+
+            return os.ToString();
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Composition_obj_exer_3.Entities;
+using Composition_obj_exer_3.Entities.Enums;
+using System.Globalization;
+
+namespace Composition_obj_exer_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            Enum.TryParse(Console.ReadLine(), out OrderStatus status);
+
+            Client c1 = new Client(name, email, date);
+            Order order = new Order(DateTime.Now, status, c1);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine();
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                 name = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product p1 = new Product(name, price);
+
+                OrderItem item = new OrderItem(quantity, price, p1);
+
+               
+
+                order.AddItem(item);
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Order SUMMARY: ");
+            Console.WriteLine(order);
+
+
+
+        }
+    }
+}
